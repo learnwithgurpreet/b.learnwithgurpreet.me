bf385d7580af4ef8702ae245cfd5faf0d7ae0ae6
Gurpreet Singh <gursingh7@publicisgroupe.net>
2023-08-14 09:27:55 +0200

    added dark colors for mobile menu

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,6 +1,9 @@
 (() => {
   document.getElementById("hamburger_menu").addEventListener("click", (e) => {
     e.target.classList.toggle("open");
+    document
+      .getElementsByTagName("body")[0]
+      .classList.toggle("overflow-hidden");
     document.getElementById("overlay-bg").classList.toggle("min-h-screen");
     document.getElementById("list-categories").classList.toggle("hidden");
   });
