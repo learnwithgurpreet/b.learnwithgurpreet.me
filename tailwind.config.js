09f01db08881070d8ee4a91a8387f4b33fc73605
Gurpreet Singh <gursingh7@publicisgroupe.net>
2024-01-09 14:48:56 +0100

    color fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./src/**/*.njk", "./src/**/*.md"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          50: "#fff0e6",
+          100: "#ffe0cc",
+          200: "#ffc299",
+          300: "#ffa366",
+          400: "#ff8533",
+          500: "#ff6600",
+          600: "#cc5200",
+          700: "#993d00",
+          800: "#662900",
+          900: "#331400",
+        },
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 };
