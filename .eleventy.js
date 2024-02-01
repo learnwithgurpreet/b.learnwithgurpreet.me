a04b919d4cda76a13472903d50fd6c9bc2b42cd8
Gurpreet Singh <gursingh7@publicisgroupe.net>
2024-02-01 14:51:04 +0100

    removed cache buster

--- .eleventy.js
+++ .eleventy.js
@@ -6,7 +6,6 @@ const LazyImages = require("./11ty/lazyLoad");
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const pluginNavigation = require("@11ty/eleventy-navigation");
-const cacheBuster = require("./11ty/cacheBuster");
 const htmlMinify = require("./11ty/htmlMinify");
 
 module.exports = function (eleventyConfig) {
@@ -26,7 +25,6 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(LazyImages, {});
   eleventyConfig.addPlugin(htmlMinify);
 
-  eleventyConfig.addFilter("cacheBuster", cacheBuster);
   eleventyConfig.addFilter("readableDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat(
       "dd MMMM yyyy"
